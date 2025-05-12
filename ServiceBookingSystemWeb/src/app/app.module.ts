851fe721435d84f6bd80f38a1d895bcebadeecbd
Sachini527 <sachinipeduruhewa@gmail.com>
2025-05-12 21:00:00 +0530

    feat: add ClientDashboard and CompanyDashboard components with templates, styles, and tests

--- ServiceBookingSystemWeb/src/app/app.module.ts
+++ ServiceBookingSystemWeb/src/app/app.module.ts
@@ -9,9 +9,10 @@ import { provideNzI18n } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { DemoNgZorroAntdModule } from './DemoNgZorroAntdModule';
 
 registerLocaleData(en);
 
@@ -24,7 +25,9 @@ registerLocaleData(en);
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    DemoNgZorroAntdModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideNzI18n(en_US),
