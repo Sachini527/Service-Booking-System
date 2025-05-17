22e6e66a6c60f436a0452f90f3261252aa11817a
Sachini527 <sachinipeduruhewa@gmail.com>
2025-05-17 23:17:22 +0530

    feat: implement signupClient method in AuthServiceImpl for client registration

--- ServiceBookingSystem/src/main/java/com/sachini527/ServiceBookingSystem/services/authentication/AuthServiceImpl.java
+++ ServiceBookingSystem/src/main/java/com/sachini527/ServiceBookingSystem/services/authentication/AuthServiceImpl.java
@@ -1,8 +1,30 @@
 package com.sachini527.ServiceBookingSystem.services.authentication;
 
+import com.sachini527.ServiceBookingSystem.dto.SignupRequestDTO;
+import com.sachini527.ServiceBookingSystem.entity.User;
+import com.sachini527.ServiceBookingSystem.enums.UserRole;
+import com.sachini527.ServiceBookingSystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 
 public class AuthServiceImpl implements AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserDto signupClient(SignupRequestDTO signupRequestDTO){
+        User user = new User();
+
+        user.setName(signupRequestDTO.getName());
+        user.setLastName(signupRequestDTO.getLastName());
+        user.setEmail(signupRequestDTO.getEmail());
+        user.setPhone(signupRequestDTO.getPhone());
+        user.setPassword(signupRequestDTO.getPassword());
+
+        user.setRole(UserRole.CLIENT);
+
+        return userRepository.save(user).getDto();
+    }
 }
