aa3a1024e6714b295c918ebd5c3dc6665271a786
Sachini527 <sachinipeduruhewa@gmail.com>
2025-05-17 20:25:40 +0530

    feat: add UserRepository interface for user data access

---
+++ ServiceBookingSystem/src/main/java/com/sachini527/ServiceBookingSystem/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sachini527.ServiceBookingSystem.repository;
+
+import com.sachini527.ServiceBookingSystem.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
