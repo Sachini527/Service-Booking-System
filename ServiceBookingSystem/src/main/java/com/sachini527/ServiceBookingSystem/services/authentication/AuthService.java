a86779584d4f298668de5772fd14c223e47cc41a
Sachini527 <sachinipeduruhewa@gmail.com>
2025-05-18 12:53:32 +0530

    feat: add signupClient method to AuthService for client registration

--- ServiceBookingSystem/src/main/java/com/sachini527/ServiceBookingSystem/services/authentication/AuthService.java
+++ ServiceBookingSystem/src/main/java/com/sachini527/ServiceBookingSystem/services/authentication/AuthService.java
@@ -1,4 +1,9 @@
 package com.sachini527.ServiceBookingSystem.services.authentication;
 
+import com.sachini527.ServiceBookingSystem.dto.SignupRequestDTO;
+import com.sachini527.ServiceBookingSystem.dto.UserDto;
+
 public interface AuthService {
+
+    UserDto signupClient(SignupRequestDTO signupRequestDTO);
 }
