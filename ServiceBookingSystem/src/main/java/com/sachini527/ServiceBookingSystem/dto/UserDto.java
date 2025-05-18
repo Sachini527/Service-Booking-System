78b26e3e1082a58f85a01efe56ead4b41dfc8100
Sachini527 <sachinipeduruhewa@gmail.com>
2025-05-18 12:47:57 +0530

    feat: expand UserDto to include additional fields and UserRole enum

--- ServiceBookingSystem/src/main/java/com/sachini527/ServiceBookingSystem/dto/UserDto.java
+++ ServiceBookingSystem/src/main/java/com/sachini527/ServiceBookingSystem/dto/UserDto.java
@@ -1,4 +1,22 @@
 package com.sachini527.ServiceBookingSystem.dto;
 
+import com.sachini527.ServiceBookingSystem.enums.UserRole;
+import lombok.Data;
+
+@Data
 public class UserDto {
+
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String lastName;
+
+    private String phone;
+
+    private UserRole role;
 }
