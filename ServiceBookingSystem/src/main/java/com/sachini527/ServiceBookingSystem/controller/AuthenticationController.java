fc72abc1f6b7c395b8d31f91810f3419f03cd70d
Sachini527 <sachinipeduruhewa@gmail.com>
2025-05-18 19:42:51 +0530

    feat: add signupClient endpoint to AuthenticationController for client registration

--- ServiceBookingSystem/src/main/java/com/sachini527/ServiceBookingSystem/controller/AuthenticationController.java
+++ ServiceBookingSystem/src/main/java/com/sachini527/ServiceBookingSystem/controller/AuthenticationController.java
@@ -1,13 +1,31 @@
 package com.sachini527.ServiceBookingSystem.controller;
 
+import com.sachini527.ServiceBookingSystem.dto.SignupRequestDTO;
+import com.sachini527.ServiceBookingSystem.dto.UserDto;
 import com.sachini527.ServiceBookingSystem.services.authentication.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class AuthenticationController {
 
     @Autowired
     private AuthService authService;
+
+    @PostMapping("/client/sign-up")
+    public ResponseEntity<?> signupClient(@RequestBody SignupRequestDTO signupRequestDTO) {
+
+        if (authService.presentByEmail(signupRequestDTO.getEmail())) {
+            return new ResponseEntity<>("Client already exists with this Email!", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        UserDto createUser = authService.signupClient(signupRequestDTO);
+
+        return new ResponseEntity<>(createUser, HttpStatus.OK);
+    }
 }
