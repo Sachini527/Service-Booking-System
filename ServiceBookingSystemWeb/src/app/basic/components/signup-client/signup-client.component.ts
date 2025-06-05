e9e5641ae75b41da1fcf3daa2035de703cdf7ed7
Sachini527 <sachinipeduruhewa@gmail.com>
2025-06-05 19:03:42 +0530

    feat: enhance SignupClientComponent with form validation and initialization

--- ServiceBookingSystemWeb/src/app/basic/components/signup-client/signup-client.component.ts
+++ ServiceBookingSystemWeb/src/app/basic/components/signup-client/signup-client.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { AuthService } from '../../services/auth/auth.service';
+import { last } from 'rxjs';
 
 @Component({
   selector: 'app-signup-client',
-  standalone: false,
   templateUrl: './signup-client.component.html',
-  styleUrl: './signup-client.component.scss'
+  styleUrls: ['./signup-client.component.scss']
 })
 export class SignupClientComponent {
 
+  validateForm!: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private notification: NzNotificationService,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.validateForm = this.fb.group({
+      email: [null, [Validators.email, Validators.required]],
+      name: [null, [Validators.required]],
+      lastName: [null, [Validators.required]],
+      phone: [null],
+      password: [null, [Validators.required]],
+      checkPassword: [null, [Validators.required]],
+    })
+  }
 }
