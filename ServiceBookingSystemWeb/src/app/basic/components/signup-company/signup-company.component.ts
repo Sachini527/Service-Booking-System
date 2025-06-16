8e3dde372242a30b930a3d71128f231fd251bb96
Sachini527 <sachinipeduruhewa@gmail.com>
2025-06-16 10:09:37 +0530

    feat: add SignupCompanyComponent with template, styles, and tests

---
+++ ServiceBookingSystemWeb/src/app/basic/components/signup-company/signup-company.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-company',
+  standalone: false,
+  templateUrl: './signup-company.component.html',
+  styleUrl: './signup-company.component.scss'
+})
+export class SignupCompanyComponent {
+
+}
